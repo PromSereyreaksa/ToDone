7f898323d8df0be1930ffc0b183068549469c9ea
Prom Sereyreaksa <161788464+PromSereyreaksa@users.noreply.github.com>
2025-03-31 01:54:44 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vitePlugin as remix } from '@remix-run/dev';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),     tailwindcss()],
+  plugins: [react(),     tailwindcss(), remix()],
 
 })
